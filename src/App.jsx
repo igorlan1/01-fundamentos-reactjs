9f98018168f008b0bdeaeb786022961999c6ddff
igor <igorlanmaq@gmail.com>
2023-09-18 17:15:29 -0300

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import {Post} from "./Post"
-import { Header } from "./components/Header"
-import "./global.css"
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post";
 
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
-    return ( 
-      <div>
-        <Header/>
-    <Post 
-    author="igor" 
-    content="lima "
-     /> 
-     <Post 
-     content="post 2"
-     />
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+       <Post/>
+       <Post/>
+        </main>
       </div>
-  )
+    </div>
+  );
 }
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
-import {Post} from "./Post.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
